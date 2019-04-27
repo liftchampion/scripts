29a4854ebb2ec1fa6168f1a64698afc40adac581
ggerardy <liftchampion@gmail.com>
2019-04-28 00:32:51 +0300

    Installer in progress - left choosing directory and set colors option

--- colorised_norm_install.sh
+++ colorised_norm_install.sh
@@ -6,13 +6,13 @@
 #    By: ggerardy <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/25 18:53:10 by ggerardy          #+#    #+#              #
-#    Updated: 2019/04/27 22:57:43 by ggerardy         ###   ########.fr        #
+#    Updated: 2019/04/28 00:31:47 by ggerardy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/usr/bin/env bash
 
-DIRNAME=~/.scripts
+DIRNAME=~/.scripts/
 ALIASNAME=norminette
 
 #############################  CHECK ZSHRC  ####################################
@@ -24,17 +24,22 @@ function check_zshrc {
 		exit 1
 	fi
 }
+################################  ASK REOPEN  ##################################
+function ask_reopen {
+	echo "Reopen your terminal, please"
+}
+
 ################  CHECK PREV ALIASES TO COLORISED NORM  ########################
 
 function find_prev_alias {
-	PREV_ALIAS_TO_THIS=$(cat ~/.zshrc | grep '^alias.\+colorised_norm\.sh')
+	PREV_ALIAS_TO_THIS=$(cat ~/.zshrc | grep '^alias.\+/colorised_norm\.sh')
 	if [[ $PREV_ALIAS_TO_THIS != "" ]]; then
 		ALIASNAME=$(echo $PREV_ALIAS_TO_THIS | sed 's/alias //g' | \
 						awk -F '=' '{print $1}')
 		DIRNAME=$(echo $PREV_ALIAS_TO_THIS | sed 's/^.*=//g' | \
 						awk -F '/' '{$NF=""; print $0}' | tr ' ' '/' | tr -d "'")
 		echo "Found \x1B[38;5;202mexisting\x1B[0m alias to this script:"
-		echo "alias \x1B[38;5;29m$ALIASNAME\x1B[0m='\x1B[38;5;29m$DIRNAME""colorised_norm.sh\x1B[0m'"
+		echo "alias \x1B[38;5;29m$ALIASNAME\x1B[0m='\x1B[38;5;29m${DIRNAME}colorised_norm.sh\x1B[0m'"
 	fi
 }
 #############################  GET ALIASNAME  ###################################
@@ -64,25 +69,57 @@ function get_alias_name {
 	fi
 }
 
+#############################  GET ALIASNAME  ###################################
+
+function get_dir_name {
+	echo "Choose directory (leave blank for '$DIRNAME') :"
+	read NEW_DIRNAME
+	NEW_DIRNAME=$(echo $NEW_DIRNAME | tr -d '\n')
+	WORDS_IN_NEW_DIRNAME=$(echo $NEW_DIRNAME | wc -w)
+	SAME_ALIASES=$(cat ~/.zshrc | grep "^alias $NEW_ALIASNAME=" | grep -v 'colorised_norm.sh')
+	echo $SAME_ALIASES
+	SAME_ALIASES_COUNT=$(echo $SAME_ALIASES | wc -w)
+	if (( $WORDS_IN_NEW_ALIASNAME > 1)) || (( $SAME_ALIASES_COUNT > 0)); then
+		echo "Bad aliasname: '$NEW_ALIASNAME'"
+		if (( $SAME_ALIASES_COUNT > 0)); then
+			echo "Alias with this name already exists"
+			echo $SAME_ALIASES
+		fi
+		printf "Try again (Y/n): "
+		read Q_RES
+		if [[ "$Q_RES" == 'n' ]] || [[ "$Q_RES" == 'N' ]]; then
+			exit 0
+		fi
+		get_alias_name
+	elif (( $WORDS_IN_NEW_ALIASNAME == 1 )) ; then
+		ALIASNAME=$NEW_ALIASNAME
+	fi
+}
+
 #########################  RM PREV ALIAS TO THIS  ###############################
 function rm_prev_alias {
 	set -e
 	cat ~/.zshrc | grep -v "$PREV_ALIAS_TO_THIS" > ~/.zshrc.backup.colorised_norm
 	cat ~/.zshrc.backup.colorised_norm > ~/.zshrc
 	rm -f ~/.zshrc.backup.colorised_norm
+	rm -f ${DIRNAME}colorised_norm.sh
+	ask_reopen
 	set +e
 }
 
 ###############################  ASK MODE  ######################################
 function ask_mode {
 	if [[ $PREV_ALIAS_TO_THIS != "" ]]; then
-		printf "Installed version found\nWould you like to Reinstall/Delete?\n[R/d]: "
+		printf "Installed version found\nWould you like to Reinstall/Delete/Exit?\n[R/d/e]: "
 		read Q_RES
 		if [[ "$Q_RES" == 'd' ]] || [[ "$Q_RES" == 'D' ]]; then
 			rm_prev_alias
-			rm -f $DIRNAME"colorised_norm.sh"
 			exit 0
 		fi
+		if [[ "$Q_RES" == 'e' ]] || [[ "$Q_RES" == 'E' ]]; then
+			exit 0
+		fi
+		rm_prev_alias
 	fi
 }
 
@@ -92,18 +129,19 @@ function install_script {
 	TMP_DIRNAME=~/.tmp_for_installing_norm_script
 	echo "\x1B[38;5;202mInstallation in progress...\x1B[0m"
 	mkdir -p $TMP_DIRNAME
+	mkdir -p $DIRNAME
 	cd $TMP_DIRNAME
 	git init --quiet
 	git remote add origin \
 			https://liftchampion@bitbucket.org/liftchampion/scripts.git 2> /dev/null
 	git fetch --quiet
 	git checkout --quiet origin/master -- colorised_norm.sh
-	mv colorised_norm.sh $DIRNAME
+	mv colorised_norm.sh "${DIRNAME}colorised_norm.sh"
 	cd $PWD
 	rm -rf $TMP_DIRNAME
-	echo "alias $ALIASNAME='$DIRNAME""colorised_norm.sh'" >> ~/.zshrc
-	source ~/.zshrc
+	echo "alias $ALIASNAME='${DIRNAME}colorised_norm.sh'" >> ~/.zshrc
 	echo "\x1B[38;5;29mDone!\x1B[0m"
+	ask_reopen
 }
 
 ####################################  MAIN  ######################################
@@ -113,7 +151,7 @@ ask_mode
 get_alias_name
 install_script
 
-echo "alias $ALIASNAME='$DIRNAME""colorised_norm.sh'"
+echo "alias $ALIASNAME='${DIRNAME}colorised_norm.sh'"
 #echo $PREV_ALIAS_TO_THIS
 #echo $PREV_ALIAS_TO_THIS_COUNT
 

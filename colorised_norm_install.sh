f4b308b7ead963086a28d682dfcca3fb3835d6c4
ggerardy <liftchampion@gmail.com>
2019-04-28 20:15:25 +0300

    Strange change in colorised_norm.sh. Installer seems to be done!

--- colorised_norm_install.sh
+++ colorised_norm_install.sh
@@ -6,7 +6,7 @@
 #    By: ggerardy <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/25 18:53:10 by ggerardy          #+#    #+#              #
-#    Updated: 2019/04/28 19:13:28 by ggerardy         ###   ########.fr        #
+#    Updated: 2019/04/28 20:13:36 by ggerardy         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,6 +14,8 @@
 
 DIRNAME=~/.scripts/
 ALIASNAME=norminette
+NORM_COLOR=29
+NO_NORM_COLOR=202
 
 #############################  CHECK ZSHRC  ####################################
 
@@ -31,6 +33,11 @@ function ask_reopen {
 
 ################  CHECK PREV ALIASES TO COLORISED NORM  ########################
 
+function get_prev_colors {
+	NORM_COLOR=$(cat ${DIRNAME}colorised_norm.sh | grep '^NORM_COLOR' | awk -F '=' '{print $2}')
+	NO_NORM_COLOR=$(cat ${DIRNAME}colorised_norm.sh | grep '^NO_NORM_COLOR' | awk -F '=' '{print $2}')
+}
+
 function find_prev_alias {
 	PREV_ALIAS_TO_THIS=$(cat ~/.zshrc | grep '^alias.\+/colorised_norm\.sh')
 	if [[ $PREV_ALIAS_TO_THIS != "" ]]; then
@@ -38,6 +45,7 @@ function find_prev_alias {
 						awk -F '=' '{print $1}')
 		DIRNAME=$(echo $PREV_ALIAS_TO_THIS | sed 's/^.*=//g' | \
 						awk -F '/' '{$NF=""; print $0}' | tr ' ' '/' | tr -d "'")
+		get_prev_colors
 		echo "Found \x1B[38;5;202mexisting\x1B[0m alias to this script:"
 		echo "alias \x1B[38;5;29m$ALIASNAME\x1B[0m='\x1B[38;5;29m${DIRNAME}colorised_norm.sh\x1B[0m'"
 	fi
@@ -128,23 +136,6 @@ function rm_prev_alias {
 	set +e
 }
 
-###############################  ASK MODE  ######################################
-function ask_mode {
-	if [[ $PREV_ALIAS_TO_THIS != "" ]]; then
-		printf "Would you like to Reinstall/Delete/Exit?\n[R/d/e]: "
-		read Q_RES
-		if [[ "$Q_RES" == 'd' ]] || [[ "$Q_RES" == 'D' ]]; then
-			rm_prev_alias
-			ask_reopen
-			exit 0
-		fi
-		if [[ "$Q_RES" == 'e' ]] || [[ "$Q_RES" == 'E' ]]; then
-			exit 0
-		fi
-		rm_prev_alias
-	fi
-}
-
 ################################  INSTALL  ######################################
 function install_script {
 	PWD=$(pwd)
@@ -166,18 +157,18 @@ function install_script {
 }
 
 ##################################  SETTINGS  ####################################
-NORM_COLOR=29
-NO_NORM_COLOR=202
 
 function print_example {
-	echo "\nCurrent colors are \x1B[38;5;${NORM_COLOR}m$NORM_COLOR\x1B[0m \x1B[38;5;${NO_NORM_COLOR}m$NO_NORM_COLOR\x1B[0m:"
-	echo "'\x1B[38;5;${NO_NORM_COLOR}mNorme: /Users/ggerardy/CLION/FDF/ft_fdf_events.c\x1B[0m"
+	echo "\nCurrent colors are \x1B[38;5;${NEW_NORM_COLOR}m$NEW_NORM_COLOR\x1B[0m" \
+			"\x1B[38;5;${NEW_NO_NORM_COLOR}m$NEW_NO_NORM_COLOR\x1B[0m:"
+	echo "'\x1B[38;5;${NEW_NO_NORM_COLOR}mNorme: /Users/ggerardy/CLION/FDF/ft_fdf_events.c\x1B[0m"
 	echo " Error: file must end with a single empty line"
-	echo " \x1B[38;5;${NORM_COLOR}mNorme: /Users/ggerardy/CLION/FDF/main.c\x1B[0m'\n"
+	echo " Error (line 127): bad indentation"
+	echo " \x1B[38;5;${NEW_NORM_COLOR}mNorme: /Users/ggerardy/CLION/FDF/main.c\x1B[0m'\n"
 }
 
 function print_colors {
-	echo "Possible colors is\n"
+	echo "Possible colors is:\n"
 	for (( b = 0; b < 16; b++ ))
 	do
 		for (( a = 1; a <= 16; a++ ))
@@ -187,6 +178,14 @@ function print_colors {
 		done
 		printf "\n"
 	done
+}
+
+function set_colors {
+	cat ${DIRNAME}colorised_norm.sh | awk -v nc=$NORM_COLOR -v nnc=$NO_NORM_COLOR \
+		'{if ($0 ~ /^NORM_COLOR=/){$0="NORM_COLOR="nc}
+		if ($0 ~ /^NO_NORM_COLOR=/){$0="NO_NORM_COLOR="nnc} print $0}' > ~/.colorised_norm_color_tmp
+	cat ~/.colorised_norm_color_tmp > ${DIRNAME}colorised_norm.sh
+	rm -f ~/.colorised_norm_color_tmp
 }
 
 function read_colors {
@@ -206,23 +205,59 @@ function read_colors {
 		then
 			echo "\x1B[38;5;160mBad color(s)\x1B[0m"
 			read_colors
+		else
+			print_example
+			printf "Comfirm? [Y/n]:"
+			read Q_RES
+			if [[ "$Q_RES" == "n" ]] || [[ "$Q_RES" == "N" ]]; then
+				read_colors
+			else
+				NORM_COLOR=$NEW_NORM_COLOR
+				NO_NORM_COLOR=$NEW_NO_NORM_COLOR
+			fi
 	fi
+}
+
+function change_color {
+	NEW_NO_NORM_COLOR=$NO_NORM_COLOR
+	NEW_NORM_COLOR=$NORM_COLOR
+	print_example
+	print_colors
+	echo ""
+	read_colors
+	set_colors
 }
 
 function ask_color_set {
 	echo "Would you like to set custom \x1B[38;5;128mcolors\x1B[0m? [y/N]"
 	read Q_RES
 	if [[ "$Q_RES" == 'y' ]] || [[ "$Q_RES" == 'Y' ]]; then
-		print_example
-		print_colors
-		echo ""
-		read_colors
+		change_color
+	fi
+}
+
+###############################  ASK MODE  ######################################
+function ask_mode {
+	if [[ $PREV_ALIAS_TO_THIS != "" ]]; then
+		printf "Would you like to Reinstall/Delete/Exit/\x1B[38;5;128mSetColors\x1B[0m?\n[R/d/e/\x1B[38;5;128ms\x1B[0m]: "
+		read Q_RES
+		if [[ "$Q_RES" == 'd' ]] || [[ "$Q_RES" == 'D' ]]; then
+			rm_prev_alias
+			ask_reopen
+			exit 0
+		fi
+		if [[ "$Q_RES" == 'e' ]] || [[ "$Q_RES" == 'E' ]]; then
+			exit 0
+		fi
+		if [[ "$Q_RES" == 's' ]] || [[ "$Q_RES" == 'S' ]]; then
+			change_color
+			exit 0
+		fi
+		rm_prev_alias
 	fi
 }
 
 ####################################  MAIN  ######################################
-ask_color_set
-exit 0
 check_zshrc
 find_prev_alias
 ask_mode
@@ -231,13 +266,3 @@ get_dir_name
 install_script
 ask_color_set
 ask_reopen
-
-echo "alias $ALIASNAME='${DIRNAME}colorised_norm.sh'"
-#echo $PREV_ALIAS_TO_THIS
-#echo $PREV_ALIAS_TO_THIS_COUNT
-
-
-
-
-
-
